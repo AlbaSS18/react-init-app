3265d046a9e7282e2ba6fef590feb50ee8ac12e7
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-04-19 16:30:46 +0200

    Componentes de clase

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import './App.css';
+import * as React from "react";
 
 const name = 'Serena';
 
 const getName = () => 'Alba';
 
 
-const ImageComp = () => {
-    const image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png';
+// Componente funcional
+const Dumbo = () => {
+    const image = 'https://img.ecartelera.com/noticias/fotos/52700/52758/1.jpg';
     return <img src={image}/>
+}
+
+// Componente de clase
+class Bambi extends React.Component {
+
+    image = 'https://media.gq.com.mx/photos/5be9e132ba170c3d2acfd96d/master/pass/bambi_2088.jpg';
+
+    render() {
+        return <img src={this.image}/>
+    }
 }
 
 function App() {
   return (
     <div className="App">
         <p>Hola {getName() + name}</p>
-        <ImageComp/>
-        <ImageComp/>
+        <Dumbo/>
+        <Bambi/>
     </div>
   );
 }

b5498cc0f7290ed9d5edc06a933ef5b3f7e19fa4
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-04-19 16:20:09 +0200

    Componentes funcionales

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ const name = 'Serena';
 
 const getName = () => 'Alba';
 
-const image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png'
+
+const ImageComp = () => {
+    const image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png';
+    return <img src={image}/>
+}
 
 function App() {
   return (
     <div className="App">
         <p>Hola {getName() + name}</p>
-        <img src={image} />
+        <ImageComp/>
+        <ImageComp/>
     </div>
   );
 }

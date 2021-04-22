7770584ed6cf6801df41418c6e6fbbc076f0f305
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-04-22 20:12:00 +0200

    UseState múltiple

--- src/Counter/index.js
+++ src/Counter/index.js
@@ -1,22 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 import ButtonCounter from '../ButtonCounter'
 import Title from '../Title'
-import {useState} from "react/cjs/react.production.min";
 
 const Counter = () => {
 
     // Eliminamos el state y lo sustituimos por esto
     const [count, setCount] = useState(0)
+    const [text, setText] = useState('No ha hecho ninguna acción');
 
 
     const handleClick = () => {
         setCount(count + 1)
-        // this.setState(prevState => ({count: prevState.count + 1}));
+        this.setState(prevState => ({count: prevState.count + 1}));
     }
 
     const handleChange = action => {
-        setCount(action === 'increment' ? count + 1 : count - 1)
+        setText(action);
+        setCount(action === 'increment' ? count + 1 : count - 1);
 
         /*this.setState(prevState =>
             ({count: action === 'increment' ? prevState.count + 1 : prevState.count -1})
@@ -31,6 +32,7 @@ const Counter = () => {
             <button onClick={handleClick}>Click me</button>
 
             <Title text={count}/>
+            <Title text={text}/>
             <ButtonCounter onPress={() => handleChange ('increment') } label="Increment" />
             <ButtonCounter onPress={() => handleChange ('decrement') }  label="Decrement" />
         </div>

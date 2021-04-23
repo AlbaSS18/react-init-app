23e7eb138611e626d8342ed232341b6f4985add5
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-04-23 21:08:57 +0200

    Animaciones

--- src/TomatoButton/index.js
+++ src/TomatoButton/index.js
@@ -1,11 +1,24 @@
 import Button from "../Button";
 import React from 'react';
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 import ButtonCounter from "../ButtonCounter";
 
+const rotate = keyframes`
+   from {
+         transform: rotate(0deg)
+   }
+   
+   to {
+         transform: rotate(360deg)
+   }
+`
 const TomatoButton = styled(ButtonCounter)`
     background-color: tomato;
-    border-color: tomato
+    border-color: tomato;
+    animation: ${rotate} 2s linear infinite
 `
+
+
+
 
 export default TomatoButton;
